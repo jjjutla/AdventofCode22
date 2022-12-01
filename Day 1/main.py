63c7e6951dd0ea847c14b798f8c2e75b38aea186
Jeevan <jeevan.jutla@gmail.com>
2022-12-01 15:01:40 +0000

    Add files via upload

---
+++ Day 1/main.py
@@ -0,0 +1,14 @@
+with open("input.txt", "r") as data:
+    totalSum = []
+    sectionSum = 0
+    for number in data:
+        if number != "\n":
+            sectionSum = sectionSum + int(number)
+        else:
+            totalSum.append(sectionSum)
+            sectionSum = 0
+totalSum.sort(reverse=True)
+print("Part 1:", totalSum[0])
+print("Part 2:", totalSum[0] + totalSum[1] + totalSum[2])
+
+
